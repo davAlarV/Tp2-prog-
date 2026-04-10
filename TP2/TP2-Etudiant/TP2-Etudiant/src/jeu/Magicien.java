1d6710a174f29e59bf989c48161076b3b002b0b7
davAlarV <psaccdavid0712@gmail.com>
2026-04-10 11:54:23 -0400

    Magicien clase

--- TP2/TP2-Etudiant/TP2-Etudiant/src/jeu/Magicien.java
+++ TP2/TP2-Etudiant/TP2-Etudiant/src/jeu/Magicien.java
@@ -1,6 +1,25 @@
 package jeu;
 
-public class Magicien  {
+public class Magicien {
+    public static final int MAX_POINTS_VIE_PERDU = 6;
     private int pointsRecuperes;
+
+    public Magicien(int pointsRecuperes, String nom) {
+        super(20, nom);
+        this.pointsRecuperes = pointsRecuperes;
+    }
+
+    @Override
+    public void joue() {
+        int pointDeViePerdu = (int) (Math.random() * MAX_POINTS_VIE_PERDU) - pointsRecuperes;
+
+        if (pointDeViePerdu < 0) pointDeViePerdu = 0;
+        setPointDeVie(getPointDeVie() - pointDeViePerdu);
+    }
+    
+    @Override
+    public String toString() {
+        return "Magicien{nom= " + nom + ", pointDeVie= " + pointDeVie + ", pointsRecuperes= " + pointsRecuperes + "}";
+    }
 
 }

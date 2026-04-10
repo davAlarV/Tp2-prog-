17ed676538fd630da4f95ec085676ab9f01a621c
davAlarV <psaccdavid0712@gmail.com>
2026-04-10 11:41:58 -0400

    Agregacion metodos diferentes

--- TP2/TP2-Etudiant/TP2-Etudiant/src/jeu/facile/JeuFacile.java
+++ TP2/TP2-Etudiant/TP2-Etudiant/src/jeu/facile/JeuFacile.java
@@ -19,6 +19,10 @@ public class JeuFacile {
 
     /*package*/ void joueJoueurFacile(String nom) {
         //TODO JF3
+        AbstractPersonnage perso = personnagesFacile.get(nom);
+        if (perso != null){
+            perso.joue();
+        }
     }
 
     /*package*/ void retireVaincuFacile() {
@@ -33,13 +37,14 @@ public class JeuFacile {
 
     public void ajouteJoueur(Joueur joueur) {
         //TODO JF2
+        joueurs.add(joueur);
     }
 
 
     public void ajoutePersonnageFacile(AbstractPersonnage perso, Joueur joueur) {
         //TODO JF2
+        personnagesFacile.put(joueur.getNom(),perso);
     }
-
 
     //Les méthodes suivatnes servent uniquement pour les tests________________________________________
 

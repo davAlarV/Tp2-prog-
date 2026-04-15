691ca0819ddbb270534655ccabef47c1c64db720
Harold Dominguez <167201862+Anonimous44@users.noreply.github.com>
2026-04-15 14:47:36 -0400

    Creation des methodes dans la classe Jeu

--- TP2/TP2-Etudiant/TP2-Etudiant/src/jeu/normal/Jeu.java
+++ TP2/TP2-Etudiant/TP2-Etudiant/src/jeu/normal/Jeu.java
@@ -11,21 +11,64 @@ public class Jeu {
 
     public Jeu() {
         //TODO J1
+        joueurs = new ArrayList<>();
+        personnages = new HashMap<>();
     }
 
 
     public void joue() {
         //TODO J3
+        int tour = 0;
+        Collections.sort(joueurs);
+        while (encoreUnVivant()) {
+            System.out.println("On joue le tour" + tour);
+            for (Joueur joueur : joueurs) {
+                System.out.println("On joue les personnages du joueur" + joueur.getNom());
+                joueJoueur(joueur.getNom());
+            }
+            retireVaincu();
+            tour++;
+        }
     }
 
 
     /*package*/ void joueJoueur(String nomJoueur) {
         //TODO J3
+        Collection<AbstractPersonnage> persos = personnages.get(nomJoueur);
+        if (persos != null) {
+            for (AbstractPersonnage perso : persos) {
+                perso.joue();
+            }
+        }
+
     }
 
 
     /*package*/ void retireVaincu() {
         //TODO J4
+        Iterator<Map.Entry<String, Collection<AbstractPersonnage>>> it =
+                personnages.entrySet().iterator();
+
+        while (it.hasNext()) {
+            Map.Entry<String, Collection<AbstractPersonnage>> entry = it.next();
+            String nomJoueur = entry.getKey();
+            Collection<AbstractPersonnage> persos = entry.getValue();
+
+            Iterator<AbstractPersonnage> itPerso = persos.iterator();
+            while (itPerso.hasNext()) {
+                AbstractPersonnage perso = itPerso.next();
+                if (perso.getPointDeVie() <= 0) {
+                    System.out.println("Le personnage " + perso.getNom()
+                            + " appartenant au joueur " + nomJoueur + " a été retiré");
+                    itPerso.remove();
+                }
+            }
+            if (persos.isEmpty()) {
+                it.remove();
+                joueurs.removeIf(j -> j.getNom().equals(nomJoueur));
+                System.out.println("Le joueur " + nomJoueur + " n'a plus de personnages");
+            }
+        }
     }
 
     //Ne pas changer cette méthde
@@ -36,11 +79,18 @@ public class Jeu {
 
     public void ajouteJoueur(Joueur joueur) {
         //TODO J2
-
+        assert joueur != null : "Joueur null";
+        joueurs.add(joueur);
     }
 
     public void ajoutePersonnage(AbstractPersonnage perso, Joueur joueur) {
         //TODO J2
+        assert perso != null : "Personnage null";
+        assert joueur != null : "Joueur null";
+        assert joueurs.contains(joueur) : "Joueur pas dans la liste";
+
+        personnages.putIfAbsent(joueur.getNom(), new HashSet<>());
+        personnages.get(joueur.getNom()).add(perso);
     }
 
 
